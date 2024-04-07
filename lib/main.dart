f153af325330356dea59eb1921d16890aa9a8748
Fatmaa17 <fatma00mahmoud0@gmail.com>
2024-04-07 21:26:37 +0200

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:therapy_app/screens/splash.dart';
+import 'package:therapy_app/screens/LoginPage.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const therapy_app());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Stateful Clicker Counter';
+class therapy_app extends StatelessWidget {
+  const therapy_app({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // Set Splash as the initial route
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Splash(),
-        '/home': (context) => MyHomePage(),
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo Click Counter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 25),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: LoginPage(),
     );
   }
 }
--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -1,1 +1,84 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
 
+import 'package:flutter/material.dart';
+import 'package:therapy_app/widgets/customtf.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFfef7ef),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/loginbg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 50), // Adjust the height here
+              Center(
+                child: Text(
+                  'TheraNest',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFe9ebc4),
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.normal,
+                    color: Color(0xFF3e3121),
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              CustomTextField(hintText: 'Email'),
+              SizedBox(height: 20),
+              CustomTextField(hintText: 'Password', obscureText: true),
+              SizedBox(height: 20), // Add space between text fields and button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF3e3121),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  width: double.infinity,
+                  height: 60,
+                  child: Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        fontFamily: 'Pacifico',
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
