9c9589ceaefbd497a09fb4dd527895a70923804e
Fatmaa17 <fatma00mahmoud0@gmail.com>
2024-04-19 23:42:06 +0200

    Adding forget pw page

---
+++ lib/features/auth/forgetPassword/view/page/forgetPW.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:therapy_app/features/auth/forgetPassword/view/component/data_widget.dart';
+
+class forget_pw extends StatefulWidget {
+  const forget_pw({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _forget_pwState();
+  }
+}
+
+class _forget_pwState extends State<forget_pw> {
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: dataWidget(),
+    );
+  }
+}
